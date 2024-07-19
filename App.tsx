b4085f00ff6febd166c33cbf02a4dc3e5deb5ca5
Chetan Waghade <75411045+wchetan@users.noreply.github.com>
2024-07-19 11:36:53 +0530

    replaced: light-content instead of dark-content

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import RootNavigation from './src/navigation/RootNavigation';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle={'dark-content'} backgroundColor={'#fff'} />
+      <StatusBar barStyle={'light-content'} backgroundColor={'#fff'} />
       <RootNavigation />
     </SafeAreaView>
   );
